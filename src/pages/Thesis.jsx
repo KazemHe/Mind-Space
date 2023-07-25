90d3a35128ca88d27fcaa9ad4e49ac441e0cf930
KazemHe <kazemhe1@gmail.com>
2023-07-25 18:03:59 +0300

    thesis

--- src/pages/Thesis.jsx
+++ src/pages/Thesis.jsx
@@ -5,26 +5,25 @@ import ThesisContent from './ThesisContent';
 
 export default function Thesis() {
   return (
-    <div className=''>
-      <div>
+    <div className='Thesis-container'>
+      <div className='Thesis-content'>
         <p>עבודת תיזה במסגרת הקורס לכתיבת תיזה פרויקט גמר</p>
       </div>
-      <div>
-        <h2>Mindspace : ידיו הוירטואליות של האדריכל</h2>
+      <div className='Thesis-content'>
+        <h1>Mindspace : ידיו הוירטואליות של האדריכל</h1>
       </div>
-      <div>
+      <div className='Thesis-content'>
         <p>מרצים: ד"ר יואב שיבר, ד"ר דקלה יזהר וד"ר רועי קוזלובסקי</p>
         תשפ"ג 2022-2023
       </div>
       <div className='Thesis-buttons'>
         <Link to='/thesis/content' className='Thesis-button'>
-          Next
+          הבא
         </Link>
         <button className='Thesis-button' onClick={() => window.history.back()}>
-          Back
+          חזרה
         </button>
       </div>
-
     </div>
   );
 }
